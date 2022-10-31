90ab67b180905a762500e27b555f81d7b18e0574
Shubham Raj <rajshubham027@gmail.com>
2022-10-31 17:32:11 +0530

    update one course controller

--- 23buildapi/main.go
+++ 23buildapi/main.go
@@ -76,7 +76,7 @@ func getOneCourse(w http.ResponseWriter, r *http.Request) {
 
 // Create one course
 func createOneCourse(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Create one courses")
+	fmt.Println("Create one course")
 	w.Header().Set("Content-Type", "application/json")
 
 	// if body is empty
@@ -98,4 +98,34 @@ func createOneCourse(w http.ResponseWriter, r *http.Request) {
 	courses = append(courses, course)
 
 	json.NewEncoder(w).Encode(courses)
+}
+
+// update one course
+func updateOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("update one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// if body is empty
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please send some data")
+	}
+
+	// get the id from request
+	params := mux.Vars(r)
+
+	// loop over the courses and find the matching id
+	for index, course := range courses {
+		if course.CourseId == params["id"] {
+			// remove the found course from courses slice
+			courses = append(courses[:index], courses[index+1:]...)
+			// create a new course with the values from the request body
+			var newCourse Course
+			_ = json.NewDecoder(r.Body).Decode(&newCourse)
+			// the id of this new course will remain same (as we're only updating)
+			newCourse.CourseId = params["id"]
+			// append the new updated course to courses slice
+			courses = append(courses, newCourse)
+			json.NewEncoder(w).Encode(newCourse)
+		}
+	}
 }
